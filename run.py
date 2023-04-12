77e37ca521a8d38cb7c7e96199d37a43bacc835e
choyoon88 <azureluster@gmail.com>
2023-04-12 19:34:27 +0000

    Build username and country input. Also avoids both inputs to be blank

--- run.py
+++ run.py
@@ -15,4 +15,13 @@ SHEET = GSPREAD_CLIENT.open('psychopath-test')
 stats = SHEET.worksheet('answers-stats')
 
 data = stats.get_all_values()
-print(data)
+
+username = input("Enter your name: ")
+while username.strip() == "":
+    username = input("Invalid input. Please enter your name: ")
+print(f"Hello, {username}!")
+
+country = input("Enter your country name: ")
+while not country.strip():
+    country = input("Invalid country name. Please enter your country: ")
+print(f"\nHello, {username} from {country}.\nLet us see if you are a psychopath from {country}\n")
