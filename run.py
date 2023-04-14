10f10386903b831dbec8d5f9c19af6bb6d9845e4
choyoon88 <azureluster@gmail.com>
2023-04-14 15:40:36 +0000

    Check if the psychopath answers are the same as the user answer and print the result

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ data = stats.get_all_values()
 questions = [
     "1) You are looking at a mirror, but unsatisfied. \nWhy is that?",
     "2) There is a portrait of a wounded solider.\nWhich part of the body is wounded?",
-    "3) You are in a dark forest alone in front of a pavilion. \nSuddenly, something passes right behind you. What could that might be?",
+    "3) You are in a dark forest alone in front of a pavilion. \nSuddenly, something passes behind you. What could that might be?",
     "4) You got very thirsty and found a vending machine. There is nothing written on the can.\nWhat is the colour of the liquid you choose?",
     "5) A murderer with a knife is looking for you in the house.\nYou are all alone and decides to hide. Where would you hide?",
     "6) You finally decide to kill your enemy you have loathe for 10 years.\nYou pick up 5 euro knife instead of 50 euro one from the store. Why is that?"]
@@ -27,7 +27,7 @@ questions = [
 options = (
     ("a. You do not like of how you look",
      "b. There us a scar on your face",
-     "c. you gained weight",
+     "c. You gained weight",
      "d. The mirror is dirty",
      "e. You found a pimple on your face"),
     ("a. Head",
@@ -48,7 +48,7 @@ options = (
     ("a. Behind the door",
      "b. Underneath the bed",
      "c. Outside the window",
-     "d.  Inside the wardrobe",
+     "d. Inside the wardrobe",
      "e. Inside the cabinet"),
     ("a. To kill with more pain",
      "b. No need to spend too much money",
@@ -57,12 +57,12 @@ options = (
      "e. Advertisement was tempting"),
 )
 
-username = input("Enter your name: ")
+username = input("Enter your name: ").capitalize()
 while username.strip() == "":
     username = input("Invalid input. Please enter your name: ")
 print(f"Hello, {username}!")
 
-country = input("Enter your country name: ")
+country = input("Enter your country name: ").capitalize()
 while not country.strip():
     country = input("Invalid input. Please enter your country: ")
 
@@ -78,14 +78,29 @@ print("Select the one that comes to your mind straight away.")
 print("DO NOT OVER THINK!")
 
 start = input("\npress Enter to contiune...")
+
 i = 0
+psycho_answers_first = ['d', 'd', 'd', 'd', 'a', 'a']
+psycho_answers_second = ['d', 'e', 'd', 'd', 'a', 'a']
+user_answers = []
+
 for question in questions:
     print('\n----------------------------------------------------------------')
     print(question)
     print('----------------------------------------------------------------')
+
     for option in options[i]:
         print(option)
     user_answer = input("Enter a, b, c, d or e: ").lower()
+
     while user_answer.lower() not in ['a', 'b', 'c', 'd', 'e']:
-        user_answer = input("Invalid answer. Please answer with a, b, c, d, e: ").lower()
+        user_answer = input("Please answer with a, b, c, d, e: ").lower()
+    user_answers.append(user_answer)
     i += 1
+
+if psycho_answers_first == user_answers or psycho_answers_second == user_answers:
+    print(f"\n{username}... You are a psychopath")
+    print(f"{country} should be warned!")
+else:
+    print(f"\n{username}... You might not be a psychopath")
+    print(f"People in {country} is safe... for now!")
